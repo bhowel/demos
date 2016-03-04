141e2b5bf4167c0d8eca2dc9b5b92e051f0122bf
Barbara Howell <bhowell@Andys-MacBook-Air.local>
2016-03-03 22:48:58 -0800

    add animation

--- createAccount/js/createAccount.js
+++ createAccount/js/createAccount.js
@@ -2,6 +2,9 @@
 	// ON DOC READY
 	
 	$( document ).ready( function() {
+		$( "#emailIcon" ).hide();
+		
+		// Test data. Comment out for production.
 		$( "#fullname" ).val( "John Smith" );
 		$( "#username" ).val( "John" );
 		$( "#email" ).val( "squigglysmith@yahoo.com" );
@@ -10,35 +13,50 @@
 		$( "#password" ).val( "password" );
 		$( "#repeatpassword" ).val( "password" );
 
+		// When user clicks Submit, validate form and send email with new account data.
 		$( "#btnSubmit" ).on( "click", function() { 
 			if ( $( "#accountForm" ).parsley().validate({}) ) {
 				if ( $( "#termsForm" ).parsley().validate({}) ) {
+				  // Play animation. Hide submit button, show email icon, play icon animation.
+					$( "#btnSubmit" ).fadeOut( "slow", function() {
+					  $( "#emailIcon" ).fadeIn( "slow", function() {
+							$( "#emailIcon" ).addClass( "magictime magic" );
 
-					var fullname = $( "#fullname" ).val();
-					var username = $( "#username" ).val();
-					var email    = $( "#email" ).val();
-					var city     = $( "#city" ).val();
-					var country  = $( "#country" ).val();
-					var password = $( "#password" ).val();
+							// Animation takes 1s. Send email after .5s.
+							setInterval( function() { 
+								sendAccountMail();
+							}, 500 );								  
+					  });
+					});				  
 
-					$.ajax({
-						type: "POST",
-						url: "sendAccountEmail.php?fullname=" + fullname + 
-																		 "&username=" + username +
-																		 "&email="    + email +
-																		 "&city="     + city +
-																		 "&country="  + country +
-																		 "&password=" + password,
-						dataType: "json",
-						complete: function( data ) {
-							window.location.href = "ack.html";
-							window.console.log( JSON.stringify( data ) );
-						}                     
-					});//ajax
-
-			
+				  				  
 				}// if termsForm validate    
 			}// if accountForm validate
 		});//btnSubmit on click
 
+		// Send email with new account data.
+		function sendAccountMail() {	    
+			var fullname = $( "#fullname" ).val();
+			var username = $( "#username" ).val();
+			var email    = $( "#email" ).val();
+			var city     = $( "#city" ).val();
+			var country  = $( "#country" ).val();
+			var password = $( "#password" ).val();
+
+			$.ajax({
+				type: "POST",
+				url: "sendAccountEmail.php?fullname=" + fullname + 
+																 "&username=" + username +
+																 "&email="    + email +
+																 "&city="     + city +
+																 "&country="  + country +
+																 "&password=" + password,
+				dataType: "json",
+				complete: function( data ) {
+					window.location.href = "ack.html";
+					window.console.log( JSON.stringify( data ) );
+				}                     
+			});//ajax
+		}//sendAccountMail	
+	
 	});// ON DOC READY
